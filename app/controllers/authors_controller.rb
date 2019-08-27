a325d5089cc4844166a2b2721f58ebf2a72980f5
Thomas Pope <tpope0928@gmail.com>
2019-08-27 23:38:40 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,19 +4,19 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
-  def posts_index
-    @author = Author.find(params[:id])
-    @posts = @author.posts
-    render template: 'posts/index'
-  end
+  # def posts_index
+  #   @author = Author.find(params[:id])
+  #   @posts = @author.posts
+  #   render template: 'posts/index'
+  # end
 
-  def post
-    @author = Author.find(params[:id])
+  # def post
+  #   @author = Author.find(params[:id])
 
-    # Note that because ids are unique by table we can go directly to
-    # Post.find — no need for @author.posts.find...
-    @post = Post.find(params[:post_id])
-    render template: 'posts/show'
+  #   # Note that because ids are unique by table we can go directly to
+  #   # Post.find — no need for @author.posts.find...
+  #   @post = Post.find(params[:post_id])
+  #   render template: 'posts/show'
   end
 
 end
